2507dbac1f4b1a77b3751f1904dcb13195e3a116
Ashen-Kingdom <opsking007@gmail.com>
2026-07-08 17:34:05 +0300

    Add WebGL renderer setup in renderer.js

---
+++ js/renderer.js
@@ -0,0 +1,11 @@
+import * as THREE from "three";
+
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+export default renderer;
